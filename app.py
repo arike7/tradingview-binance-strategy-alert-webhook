e1401ecac5f083f0cdbb5401c0d2ab47fc602c62
arike7 <ara11055928@gmail.com>
2022-09-03 03:40:33 +0600

    spot

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
     side = data['strategy']['order_action'].upper()
     quantity = data['strategy']['order_contracts']
-    order_response = order(side, quantity, "ETHPERP")
+    order_response = order(side, quantity, "ETHUSDT")
     
     if order_response:
         return {
